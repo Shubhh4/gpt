1d29aca7ae4445763399f2aabd03fd025b278c52
Shubham Pathak <95904081+Shubhh4@users.noreply.github.com>
2023-06-04 22:27:04 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -7,7 +7,7 @@ const chatContainer = document.querySelector('#chat_container')
 let loadInterval
 
 function loader(element) {
-    element.textContent = ''
+    element.textContennt = ''
 
     loadInterval = setInterval(() => {
         // Update the text content of the loading indicator
